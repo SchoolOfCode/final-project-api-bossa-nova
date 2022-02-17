20000217570bdbe7d9509c062fefb7798dc6afa1
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-17 10:42:37 +0000

    connected to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import dotenv from "dotenv/config";
 import express from "express";
-
+import mongoose from "mongoose";
 import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 app.use(cors());
