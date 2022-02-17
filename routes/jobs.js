6f8a3c85f9e332139ecbf344ced323495fd8576a
drinobre <dri_nobre@hotmail.com>
2022-02-17 15:27:49 +0000

    getJobByID and patch request

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,6 +8,10 @@ router.get("/", async (req, res) => {
   res.json(jobs);
 });
 
+router.get("/:id", getJobByID, async (req, res) => {
+  res.json(res.job);
+});
+
 router.post("/", async (req, res) => {
   const job = new Job({
     userID: req.body.userID,
@@ -31,5 +35,20 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// middleware function to select job by id
+
+async function getJobByID(req, res, next) {
+  let correctJob;
+
+  try {
+    correctJob = await Job.findById(req.params.id);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.job = correctJob;
+  next();
+}
 
 export default router;
