26fc053218f338afc65887feff543360a87822f7
kattana <kattanaluz@gmail.com>
2022-02-17 11:24:52 +0000

    created post router

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,4 +8,18 @@ router.get("/", async (req, res) => {
   res.json(jobs);
 });
 
+router.post("/", async (req, res) => {
+  const job = new Job({
+    userID: req.body.userID,
+    jobTitle: req.body.jobTitle,
+    company: req.body.company,
+    jobStatus: req.body.jobStatus,
+  });
+  try {
+    const newJob = await job.save();
+    res.status(201).json(newJob);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 export default router;
