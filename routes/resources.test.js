13d5982e6ce34bb0b792038eaf7a53d48e039c1e
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-04 15:49:27 +0000

    post request tests done

--- routes/resources.test.js
+++ routes/resources.test.js
@@ -3,14 +3,32 @@ import request from "supertest";
 // import { db } from "../app.js";
 
 describe("GET /resources", function () {
-  it("get/api/resources with correct response", async function () {
+  it("GET api/resources with correct response", async function () {
     const response = await request(app)
       .get("/api/resources")
       .set("Accept", "application/json");
-    console.log(response.body);
     expect(response.status).toEqual(200);
     expect(response.body.success).toBeTruthy();
     expect(Array.isArray(response.body.payload)).toBeTruthy();
+  });
+});
+
+describe("POST /resources", function () {
+  it("POST api/resources - good request", async function () {
+    const response = await request(app)
+      .post("/api/resources")
+      .send({ link: "https://www.facebook.com" });
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBeTruthy();
+    expect(typeof response.body.payload).toBe("object");
+  });
+
+  it("POST api/resources - bad request", async function () {
+    const response = await request(app)
+      .post("/api/resources")
+      .send({ link: 1234 });
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBeFalsy();
   });
 });
 
