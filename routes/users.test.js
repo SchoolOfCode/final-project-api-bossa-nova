44edbf498990383713133f99852c33ed4acf3033
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-07 11:08:45 +0000

    test find user by id

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,5 +1,6 @@
 import app from "../app";
 import request from "supertest";
+import { getAllUsers } from "../helpers/users.js";
 
 describe("GET /user", function () {
   it("GET api/user", async function () {
@@ -31,6 +32,22 @@ describe("POST /user", function () {
       maxSalary: "30000",
     });
     expect(response.status).toEqual(400);
+    expect(response.body.success).toBeFalsy();
+  });
+});
+
+describe("GET api/user/:id - get one user", function () {
+  it("GET api/user/:id - user found", async function () {
+    const response = await request(app).get("/api/user/auth0_user");
+    expect(response.status).toEqual(200);
+    expect(response.body.success).toBeTruthy();
+    expect(Array.isArray(response.body.payload)).toBeTruthy();
+    expect(response.body.payload.length).toBe(1);
+  });
+
+  it("GET api/user/:id - user NOT found", async function () {
+    const response = await request(app).get("/api/user/not_a_user");
+    expect(response.status).toEqual(404);
     expect(response.body.success).toBeFalsy();
   });
 });
