deb4b6ed210a49a8c61cc097ca7423950a19d161
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-07 11:31:54 +0000

    tests for put requests

--- routes/users.test.js
+++ routes/users.test.js
@@ -1,6 +1,6 @@
 import app from "../app";
 import request from "supertest";
-import { getAllUsers } from "../helpers/users.js";
+import { getUsersByID } from "../helpers/users";
 
 describe("GET /user", function () {
   it("GET api/user", async function () {
@@ -48,6 +48,64 @@ describe("GET api/user/:id - get one user", function () {
   it("GET api/user/:id - user NOT found", async function () {
     const response = await request(app).get("/api/user/not_a_user");
     expect(response.status).toEqual(404);
+    expect(response.body.success).toBeFalsy();
+  });
+});
+
+describe("GET api/user/:user_id/:job_id - get one job from one user", function () {
+  it("GET api/user/:user_id/:job_id - user found - job found", async function () {
+    const user = await getUsersByID("auth0_user");
+    const jobs = user[0].jobs;
+    const job_id = String(jobs[jobs.length - 1]._id);
+    const response = await request(app).get(`/api/user/auth0_user/${job_id}`);
+    expect(response.status).toEqual(200);
+    expect(response.body.success).toBeTruthy();
+    expect(Array.isArray(response.body.payload)).toBeTruthy();
+    expect(response.body.payload.length).toBe(1);
+  });
+
+  it("GET api/user/:user_id/:job_id - user not found", async function () {
+    const response = await request(app).get("/api/user/not_a_user/not_a_job");
+    expect(response.status).toEqual(404);
+    expect(response.body.success).toBeFalsy();
+  });
+});
+
+describe("PUT api/user/:user_id/:job_id - update one job for one user", function () {
+  it("PUT api/user/:user_id/:job_id - update job - good request", async function () {
+    const user = await getUsersByID("auth0_user");
+    const jobs = user[0].jobs;
+    const job_id = String(jobs[jobs.length - 1]._id);
+    const response = await request(app)
+      .put(`/api/user/auth0_user/${job_id}`)
+      .send({
+        jobTitle: "Junior Dev",
+        company: "Google",
+        jobStatus: "Interview",
+        minSalary: 22000,
+        maxSalary: 30000,
+        location: "London",
+        notes: "a good job",
+      });
+    expect(response.status).toEqual(200);
+    expect(response.body.success).toBeTruthy();
+    expect(typeof response.body.payload).toBe("object");
+    expect(response.body.payload._id).toEqual("auth0_user");
+  });
+  it("PUT api/user/:user_id/:job_id - update job - bad request", async function () {
+    const user = await getUsersByID("auth0_user");
+    const jobs = user[0].jobs;
+    const job_id = String(jobs[jobs.length - 1]._id);
+    const response = await request(app)
+      .put(`/api/user/auth0_user/${job_id}`)
+      .send({
+        jobStatus: 123,
+        minSalary: "I dont know",
+        maxSalary: "I wish it was more",
+        location: "London",
+        notes: "a good job",
+      });
+    expect(response.status).toEqual(400);
     expect(response.body.success).toBeFalsy();
   });
 });
